a26a731cba6bc70f4faf44d5e05d7b3abb055a01
Nasser <nasser.kessas@outlook.com>
2023-03-03 09:13:58 +1000

    Added websocket server

---
+++ websocket/server.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const ws = new require('ws');
+
+const wss = new ws.Server({ noServer: true });
+
+const clients = new Set();
+
+function accept(req, res) {
+
+  if (req.url == '/ws' && req.headers.upgrade &&
+    req.headers.upgrade.toLowerCase() == 'websocket' &&
+    // can be Connection: keep-alive, Upgrade
+    req.headers.connection.match(/\bupgrade\b/i)) {
+    wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
+  } else { // page not found
+    res.writeHead(404);
+    res.end();
+  }
+}
+
+function onSocketConnect(ws) {
+  clients.add(ws);
+  log(`new connection`);
+
+  ws.on('message', function (message) {
+    log(`message received: ${message}`);
+
+    message = message.slice(0, 50); // max message length will be 50
+
+    for (let client of clients) {
+      client.send(message);
+    }
+  });
+
+  ws.on('close', function () {
+    log(`connection closed`);
+    clients.delete(ws);
+  });
+}
+
+let log = console.log;
+
+http.createServer(accept).listen(8080);
