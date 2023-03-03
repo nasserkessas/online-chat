a70ea21ebdd640d73a284997306031bccddd67cf
Nasser <nasser.kessas@outlook.com>
2023-03-03 11:28:28 +1000

    Added code generation

--- websocket/http.js
+++ websocket/http.js
@@ -3,9 +3,28 @@ const http = require("http");
 const host = 'localhost';
 const port = 8000;
 
+const codeLen = 8;
+
+
 const requestListener = function (req, res) {
     res.setHeader("Content-Type", "application/json");
+    switch (req.url){
+        case "/code":
+            if (req.method == "GET"){
+                res.writeHead(200);
+                res.end(`{"code": "${generateCode()}"}`);
+	    }
+            else {
+                res.writeHead(404);
+                res.end(`{"error": "Cannot ${req.method} \"${req.url}\""}`)
+	    }
+            break;
+    }
 };
+
+const generateCode = () => {
+    return "abcdefghijklmnopqrstuvwxyzABSCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split('').sort(function(){return 0.5-Math.random()}).join('').slice(0,codeLen);
+}
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
