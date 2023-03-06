8b5e165d4c7305bfbb6111bac18263429c607b04
Nasser <nasser.kessas@outlook.com>
2023-03-06 14:26:25 +1000

    Improved websocket server to be more clear

--- websocket/server.js
+++ websocket/server.js
@@ -1,28 +1,16 @@
-import { createServer } from 'http';
 import { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({ noServer: true });
+const wss = new WebSocketServer({ port: 8080 });
 
 const clients = new Set();
 
-function accept(req, res) {
-
-  if (req.url == '/ws' && req.headers.upgrade &&
-    req.headers.upgrade.toLowerCase() == 'websocket' &&
-    // can be Connection: keep-alive, Upgrade
-    req.headers.connection.match(/\bupgrade\b/i)) {
-    wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
-  } else { // page not found
-    res.writeHead(404);
-    res.end();
-  }
-}
-
-function onSocketConnect(ws) {
-  clients.add(ws);
+wss.on("connection", (socket, req) => {
+  
   log(`new connection`);
+  log(req.url);
+  clients.add(socket);
 
-  ws.on('message', function (message) {
+  socket.on('message', function (message) {
     log(`message received: ${message}`);
 
     message = message.slice(0, 50); // max message length will be 50
@@ -32,12 +20,10 @@ function onSocketConnect(ws) {
     }
   });
 
-  ws.on('close', function () {
+  socket.on('close', function () {
     log(`connection closed`);
-    clients.delete(ws);
+    clients.delete(socket);
   });
-}
+})
 
 let log = console.log;
-
-createServer(accept).listen(8080);
