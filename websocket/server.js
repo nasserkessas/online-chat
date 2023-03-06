02721d1aaf9f6ea1b1e02a75b56fe2376c7b0a7a
Nasser <nasser.kessas@outlook.com>
2023-03-06 14:54:15 +1000

    Made messages send only to users with the same code

--- websocket/server.js
+++ websocket/server.js
@@ -2,27 +2,32 @@ import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
-const clients = new Set();
+let clients = [];
 
 wss.on("connection", (socket, req) => {
   
   log(`new connection`);
-  log(req.url);
-  clients.add(socket);
+
+  clients.push({socket, code: req.url.slice(1)});
+  log(clients.length);
+
 
   socket.on('message', function (message) {
     log(`message received: ${message}`);
 
     message = message.slice(0, 50); // max message length will be 50
 
-    for (let client of clients) {
-      client.send(message);
+    const thisCode = clients.find(c => c.socket === socket).code;
+
+    for (let client of clients.filter(c => c.code === thisCode)) {
+      client.socket.send(message);
     }
   });
 
   socket.on('close', function () {
     log(`connection closed`);
-    clients.delete(socket);
+    clients = clients.filter(c => c.socket != socket);
+    log(clients.length);
   });
 })
 
